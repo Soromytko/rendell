33ad51f79d4d7e95b999dbab2015fb55cc76e4fb
Soromytko <NikitaSoromytko@gmail.com>
2024-11-24 18:12:48 +0300

    Fix namespace

--- src/OpenGL/OpenGLShaderProgram.cpp
+++ src/OpenGL/OpenGLShaderProgram.cpp
@@ -1,117 +1,117 @@
 #include "OpenGLShaderProgram.h"
 #define LOG_INFO_BUFFER_SIZE 512
 
-using namespace rendell;
-
-static GLuint createShader(const std::string& src, GLuint type)
+namespace rendell
 {
-	GLuint result = glCreateShader(type);
-	const GLchar* srcPtr = static_cast<const GLchar*>(src.c_str());
-	glShaderSource(result, 1, &srcPtr, NULL);
-	return result;
-}
-
-OpenGLShaderProgram::OpenGLShaderProgram(std::string&& vertexSrc, std::string&& fragmentSrc) :
-	ShaderProgram(std::move(vertexSrc), std::move(fragmentSrc))
-{
-	_vertexShaderId = createShader(_vertexSrc, GL_VERTEX_SHADER);
-	_fragmentShaderId = createShader(_fragmentSrc, GL_FRAGMENT_SHADER);
-	_shaderProgramId = glCreateProgram();
-}
-
-OpenGLShaderProgram::~OpenGLShaderProgram()
-{
-	glDeleteShader(_vertexShaderId);
-	glDeleteShader(_fragmentShaderId);
-	glDeleteProgram(_shaderProgramId);
-}
-
-void OpenGLShaderProgram::setVertexShaderSrc(std::string&& vertexSrc)
-{
-	ShaderProgram::setVertexShaderSrc(std::move(vertexSrc));
-	glDeleteShader(_vertexShaderId);
-	_vertexShaderId = createShader(_vertexSrc, GL_VERTEX_SHADER);
-}
-
-void OpenGLShaderProgram::setFragmentShaderSrc(std::string&& fragmentSrc)
-{
-	ShaderProgram::setFragmentShaderSrc(std::move(fragmentSrc));
-	glDeleteShader(_fragmentShaderId);
-	_fragmentShaderId = createShader(_fragmentSrc, GL_FRAGMENT_SHADER);
-}
-
-static bool IsShaderCompiledSuccessfully(GLuint shaderId, std::string* infoLog)
-{
-	int success;
-	glCompileShader(shaderId);
-	glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
-	if (!success && infoLog)
+	static GLuint createShader(const std::string& src, GLuint type)
 	{
-		char infoLogBuffer[LOG_INFO_BUFFER_SIZE];
-		glGetShaderInfoLog(shaderId, LOG_INFO_BUFFER_SIZE, NULL, infoLogBuffer);
-		*infoLog = infoLogBuffer;
+		GLuint result = glCreateShader(type);
+		const GLchar* srcPtr = static_cast<const GLchar*>(src.c_str());
+		glShaderSource(result, 1, &srcPtr, NULL);
+		return result;
 	}
-	return success;
-}
 
-bool OpenGLShaderProgram::compile(std::string* vertexInfoLog, std::string* fragmentInfoLog) const
-{
-	const bool vertexResult = IsShaderCompiledSuccessfully(_vertexShaderId, vertexInfoLog);
-	const bool fragmentResult = IsShaderCompiledSuccessfully(_fragmentShaderId, fragmentInfoLog);
-	return vertexResult && fragmentResult;
-}
-
-bool OpenGLShaderProgram::link(std::string* infoLog) const
-{
-	glAttachShader(_shaderProgramId, _vertexShaderId);
-	glAttachShader(_shaderProgramId, _fragmentShaderId);
-	glLinkProgram(_shaderProgramId);
-
-	int success;
-	glGetProgramiv(_shaderProgramId, GL_LINK_STATUS, &success);
-	if (!success && infoLog)
+	OpenGLShaderProgram::OpenGLShaderProgram(std::string&& vertexSrc, std::string&& fragmentSrc) :
+		ShaderProgram(std::move(vertexSrc), std::move(fragmentSrc))
 	{
-		char infoLogBuffer[LOG_INFO_BUFFER_SIZE];
-		glGetProgramInfoLog(_shaderProgramId, LOG_INFO_BUFFER_SIZE, NULL, infoLogBuffer);
-		*infoLog = infoLogBuffer;
+		_vertexShaderId = createShader(_vertexSrc, GL_VERTEX_SHADER);
+		_fragmentShaderId = createShader(_fragmentSrc, GL_FRAGMENT_SHADER);
+		_shaderProgramId = glCreateProgram();
 	}
-	return success;
-}
 
-void OpenGLShaderProgram::freeSrc()
-{
-	glDeleteShader(_vertexShaderId);
-	glDeleteShader(_fragmentShaderId);
-	_vertexSrc.clear();
-	_fragmentSrc.clear();
-}
+	OpenGLShaderProgram::~OpenGLShaderProgram()
+	{
+		glDeleteShader(_vertexShaderId);
+		glDeleteShader(_fragmentShaderId);
+		glDeleteProgram(_shaderProgramId);
+	}
 
-void OpenGLShaderProgram::bind() const
-{
-	glUseProgram(_shaderProgramId);
-}
+	void OpenGLShaderProgram::setVertexShaderSrc(std::string&& vertexSrc)
+	{
+		ShaderProgram::setVertexShaderSrc(std::move(vertexSrc));
+		glDeleteShader(_vertexShaderId);
+		_vertexShaderId = createShader(_vertexSrc, GL_VERTEX_SHADER);
+	}
 
-void OpenGLShaderProgram::unbind() const
-{
-	glUseProgram(0);
-}
+	void OpenGLShaderProgram::setFragmentShaderSrc(std::string&& fragmentSrc)
+	{
+		ShaderProgram::setFragmentShaderSrc(std::move(fragmentSrc));
+		glDeleteShader(_fragmentShaderId);
+		_fragmentShaderId = createShader(_fragmentSrc, GL_FRAGMENT_SHADER);
+	}
 
-uint32_t OpenGLShaderProgram::getUniformIndex(const std::string& uniformName) const
-{
-	return glGetUniformBlockIndex(
-		_shaderProgramId,
-		static_cast<const GLchar*>(uniformName.c_str())
-	);
-}
+	static bool IsShaderCompiledSuccessfully(GLuint shaderId, std::string* infoLog)
+	{
+		int success;
+		glCompileShader(shaderId);
+		glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
+		if (!success && infoLog)
+		{
+			char infoLogBuffer[LOG_INFO_BUFFER_SIZE];
+			glGetShaderInfoLog(shaderId, LOG_INFO_BUFFER_SIZE, NULL, infoLogBuffer);
+			*infoLog = infoLogBuffer;
+		}
+		return success;
+	}
 
-void OpenGLShaderProgram::setUniformBindings(const std::string& uniformName, uint32_t dataBinding)
-{
-	const uint32_t index = getUniformIndex(uniformName);
-	setUniformBindings(index, dataBinding);
-}
+	bool OpenGLShaderProgram::compile(std::string* vertexInfoLog, std::string* fragmentInfoLog) const
+	{
+		const bool vertexResult = IsShaderCompiledSuccessfully(_vertexShaderId, vertexInfoLog);
+		const bool fragmentResult = IsShaderCompiledSuccessfully(_fragmentShaderId, fragmentInfoLog);
+		return vertexResult && fragmentResult;
+	}
 
-void OpenGLShaderProgram::setUniformBindings(uint32_t uniformIndex, uint32_t dataBinding)
-{
-	glUniformBlockBinding(_shaderProgramId, uniformIndex, dataBinding);
-}
+	bool OpenGLShaderProgram::link(std::string* infoLog) const
+	{
+		glAttachShader(_shaderProgramId, _vertexShaderId);
+		glAttachShader(_shaderProgramId, _fragmentShaderId);
+		glLinkProgram(_shaderProgramId);
 
+		int success;
+		glGetProgramiv(_shaderProgramId, GL_LINK_STATUS, &success);
+		if (!success && infoLog)
+		{
+			char infoLogBuffer[LOG_INFO_BUFFER_SIZE];
+			glGetProgramInfoLog(_shaderProgramId, LOG_INFO_BUFFER_SIZE, NULL, infoLogBuffer);
+			*infoLog = infoLogBuffer;
+		}
+		return success;
+	}
+
+	void OpenGLShaderProgram::freeSrc()
+	{
+		glDeleteShader(_vertexShaderId);
+		glDeleteShader(_fragmentShaderId);
+		_vertexSrc.clear();
+		_fragmentSrc.clear();
+	}
+
+	void OpenGLShaderProgram::bind() const
+	{
+		glUseProgram(_shaderProgramId);
+	}
+
+	void OpenGLShaderProgram::unbind() const
+	{
+		glUseProgram(0);
+	}
+
+	uint32_t OpenGLShaderProgram::getUniformIndex(const std::string& uniformName) const
+	{
+		return glGetUniformBlockIndex(
+			_shaderProgramId,
+			static_cast<const GLchar*>(uniformName.c_str())
+		);
+	}
+
+	void OpenGLShaderProgram::setUniformBindings(const std::string& uniformName, uint32_t dataBinding)
+	{
+		const uint32_t index = getUniformIndex(uniformName);
+		setUniformBindings(index, dataBinding);
+	}
+
+	void OpenGLShaderProgram::setUniformBindings(uint32_t uniformIndex, uint32_t dataBinding)
+	{
+		glUniformBlockBinding(_shaderProgramId, uniformIndex, dataBinding);
+	}
+}
--- src/VertexBuffer.cpp
+++ src/VertexBuffer.cpp
@@ -1,43 +1,43 @@
 #include "VertexBuffer.h"
 #include "OpenGL/OpenGLVertexBuffer.h"
 
-using namespace rendell;
-
-VertexBuffer::VertexBuffer(std::vector<float>&& data)
+namespace rendell
 {
-	_data = std::move(data);
-}
-
-const std::vector<float>& VertexBuffer::getData() const
-{
-	return _data;
-}
-
-int VertexBuffer::getStride() const
-{
-	return _stride;
-}
-
-const std::vector<VertexBufferLayout>& VertexBuffer::getLayouts() const
-{
-	return _layouts;
-}
-
-void VertexBuffer::setLayouts(const std::vector<VertexBufferLayout>& layouts)
-{
-	_layouts = layouts;
-	calculateOffsetsAndStride();
-}
-
-void VertexBuffer::calculateOffsetsAndStride()
-{
-	uint32_t offset = 0;
-	_stride = 0;
-	for (VertexBufferLayout& layout : _layouts)
+	VertexBuffer::VertexBuffer(std::vector<float>&& data)
 	{
-		layout.setOffset(offset);
-		offset += layout.getSize();
-		_stride += layout.getSize();
+		_data = std::move(data);
+	}
+
+	const std::vector<float>& VertexBuffer::getData() const
+	{
+		return _data;
+	}
+
+	int VertexBuffer::getStride() const
+	{
+		return _stride;
+	}
+
+	const std::vector<VertexBufferLayout>& VertexBuffer::getLayouts() const
+	{
+		return _layouts;
+	}
+
+	void VertexBuffer::setLayouts(const std::vector<VertexBufferLayout>& layouts)
+	{
+		_layouts = layouts;
+		calculateOffsetsAndStride();
+	}
+
+	void VertexBuffer::calculateOffsetsAndStride()
+	{
+		uint32_t offset = 0;
+		_stride = 0;
+		for (VertexBufferLayout& layout : _layouts)
+		{
+			layout.setOffset(offset);
+			offset += layout.getSize();
+			_stride += layout.getSize();
+		}
 	}
 }
-
