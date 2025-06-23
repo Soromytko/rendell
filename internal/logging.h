70635a2a9c09bb9aa53f32cf2bb78b0e6a218024
Soromytko <NikitaSoromytko@gmail.com>
2025-06-24 00:53:14 +0300

    Fix for Linux build

--- internal/logging.h
+++ internal/logging.h
@@ -17,9 +17,9 @@ namespace rendell
 	logx::Logger* get_logger();
 }
 
-#define RENDELL_CRITICAL(formatStr, ...) rendell::get_logger()->critical(std::format(formatStr, __VA_ARGS__))
-#define RENDELL_ERROR(formatStr, ...) rendell::get_logger()->error(std::format(formatStr, __VA_ARGS__))
-#define RENDELL_WARNING(format, ...) rendell::get_logger()->warning(std::format(formatStr, __VA_ARGS__))
-#define RENDELL_INFO(formatStr, ...) rendell::get_logger()->info(std::format(formatStr, __VA_ARGS__))
-#define RENDELL_DEBUG(formatStr, ...) rendell::get_logger()->debug(std::format(formatStr, __VA_ARGS__))
-#define RENDELL_TRACE(formatStr, ...) rendell::get_logger()->trace(std::format(formatStr, __VA_ARGS__))
+#define RENDELL_CRITICAL(formatStr, ...) rendell::get_logger()->critical(std::format(formatStr, ##__VA_ARGS__))
+#define RENDELL_ERROR(formatStr, ...) rendell::get_logger()->error(std::format(formatStr, ##__VA_ARGS__))
+#define RENDELL_WARNING(formatStr, ...) rendell::get_logger()->warning(std::format(formatStr, ##__VA_ARGS__))
+#define RENDELL_INFO(formatStr, ...) rendell::get_logger()->info(std::format(formatStr, ##__VA_ARGS__))
+#define RENDELL_DEBUG(formatStr, ...) rendell::get_logger()->debug(std::format(formatStr, ##__VA_ARGS__))
+#define RENDELL_TRACE(formatStr, ...) rendell::get_logger()->trace(std::format(formatStr, ##__VA_ARGS__))
